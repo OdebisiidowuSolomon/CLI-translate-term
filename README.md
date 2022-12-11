793600839c03df30f1804b5614219e2eaf2692dc
unknown <odebisiidowu09@gmail.com>
2022-12-11 14:29:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+  # CLI-Translate-term v1.0.0
+ <p align="center">
+   <img  src="https://img.shields.io/badge/license-MIT-green">
+ <img  src="https://img.shields.io/badge/build-passing-brightgreen">
+   <img  src="https://img.shields.io/badge/version-1.0.0-orange">
+   <img  src="https://img.shields.io/badge/npm-v9.2.0-blue">
+  <img  src="https://img.shields.io/badge/node-v16.17.0-yellow">
+ </p>
+ <br>
+<p align="center">A simple CLI to translate to any specific language in the terminal.</p>
+<br>
+
+<p align="center">
+<img src="images/demo2.gif" alt="demonstration" height="500" width="800" >
+</p>
+
+
+---
+
+## Usage
+
+```
+~$ tran --help
+Usage: tran <lang_name> <sentence to be translated>
+
+Options:
+        --version             Show version number.              [boolean]
+    -l, --languages           List all languages.               [boolean]
+        --help                Show help.                        [boolean]
+
+```
+
+> :warning: **You can also use the ISO-639-1 code of the language instead of writing the full language name**
+
+<br>
+
+## Installation
+
+1. Clone the repository and then navigate to it.
+2. Run ```npm install``` to install the dependencies.
+3. Run ```npm install -g .``` to install the CLI. <br>
+
+4. Now you are good to go and can use the CLI globally!
+
+Type ```tran``` or ```tran --help``` to get started.
+
+<br>
+
+## License
+
+MIT ©***cli-translate-term***
---
+++ bin/index.js
@@ -0,0 +1,95 @@
+#! /usr/bin/env node
+
+const chalk = require("chalk");
+const boxen = require("boxen");
+const ora = require("ora");
+const utils = require("./utils.js");
+const translate = require("@vitalets/google-translate-api");
+const usage = chalk.keyword("violet")(
+  "\nUsage: tran <lang_name> sentence to be translated"
+);
+
+const yargs = require("yargs");
+const { getSpecificLang } = require("./utils.js");
+
+const options = yargs
+  .usage(usage)
+  .option("l", {
+    alias: "languages",
+    describe: "List all supported languages.",
+    type: "boolean",
+    demandOption: false,
+  })
+  .help(true).argv;
+
+if (yargs.argv.l == true || yargs.argv.languages == true) {
+  const spinner = ora("Loading languages").start();
+
+  setTimeout(() => {
+    spinner.color = "yellow";
+    spinner.text = "Languages Loaded!";
+    spinner.succeed();
+    utils.showAll();
+  }, 500);
+
+  return;
+}
+
+if (yargs.argv._[0] == null) {
+  utils.showHelp();
+  return;
+}
+
+if (yargs.argv._[0]) {
+  let language = yargs.argv._[0].toLowerCase(); // stores the language.
+//   console.log(yargs.argv);
+
+  let sentence = "";
+
+  //parsing the sentence to be translated.
+  sentence = utils.parseSentence(yargs.argv._);
+
+  if (sentence == "") {
+    console.error(
+      chalk.red.bold(
+        "\nThe entered sentence is like John Cena, I can't see it!\n"
+      )
+    );
+    console.log(chalk.green("Enter tran --help to get started.\n"));
+    return;
+  }
+
+  //parsing the language specified to the ISO-639-1 code.
+  language = utils.parseLanguage(language);
+
+  const spinner = ora(`Translating language to ${language}`).start();
+
+  //terminating the program if the language is unsupported.
+  if (language == null) {
+    spinner.color = "red";
+    spinner.text = "Languages is not supported!";
+    spinner.fail();
+    return;
+  }
+
+  translate(sentence, { to: language })
+    .then((res) => {
+      spinner.color = "green";
+      spinner.text = "Language translated!";
+      spinner.succeed();
+      console.log(
+        "\n" +
+          boxen(chalk.green("\n" + res.text + "\n"), {
+            padding: 1,
+            borderColor: "green",
+            dimBorder: true,
+          }) +
+          "\n"
+      );
+    })
+    .catch((err) => {
+      spinner.color = "yellow";
+      spinner.text = `Some Error Occured!: Error - '${err.code}'`;
+      spinner.fail();
+    });
+}
